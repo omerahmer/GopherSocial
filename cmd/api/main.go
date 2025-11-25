12f03115f49321036d61b7e6db5de2812d40b646
omerahmer <omerahmer918@gmail.com>
2025-11-24 23:47:57 -0800

    starting repository pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	"github.com/omerahmer/GopherSocial/internal/env"
+	"github.com/omerahmer/GopherSocial/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
